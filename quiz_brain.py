a3a3d856035874bf85d035acaf77ed41d04b1ec6
youssefahmed2017 <youssef.ahmed.29062017@gmail.com>
2026-04-17 22:20:39 +0200

    Create quiz_brain.py

---
+++ quiz_brain.py
@@ -0,0 +1,10 @@
+import main
+class QuizBrain:
+    def __init__(self):
+        question_list = []
+        for question in main.questionbank:
+            self.question = main.questionbank
+            aa = question["text"]
+            question_list.append(aa)
+        question_number = 0
+q = QuizBrain()
